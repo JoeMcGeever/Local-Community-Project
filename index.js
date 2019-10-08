56023a8a5b48f90e8e5971ef7692d69702ab5ed1
Joseph McGeever <mcgeevej@coventry.ac.uk>
2019-10-08 13:52:53 +0100

    Fixed the registering problem

--- index.js
+++ index.js
@@ -70,17 +70,15 @@ router.get('/register', async ctx => await ctx.render('register'))
  * @name Register Script
  * @route {POST} /register
  */
-router.post('/register', koaBody, async ctx => {
+router.post('/registerPost', koaBody, async ctx => {
 	try {
 		// extract the data from the request
 		const body = ctx.request.body
 		console.log(body)
-		const {path, type} = ctx.request.files.avatar
+
 		// call the functions in the module
 		const user = await new User(dbName)
 		await user.register(body.user, body.pass, body.address, body.postcode, body.ward, body.email)
-		// await user.uploadPicture(path, type)
-		// redirect to the home page
 		ctx.redirect(`/?msg=new user "${body.name}" added`)
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
